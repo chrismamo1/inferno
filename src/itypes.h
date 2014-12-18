d9b5bdd815e55520a7debd6868c883ec5306fb2e
chrismamo1 <christophermcalpine@gmail.com>
2014-12-17 20:34:08 -0600

    Finally fixed it (yay for inferno compiling now!) I also added a couple of unit tests and converted some utility functions to preprocessor macros. The region system is now mostly complete.

---
+++ src/itypes.h
@@ -0,0 +1,9 @@
+#ifndef _ITYPES_H
+#define _ITYPES_H
+typedef unsigned char uint8;
+typedef unsigned char ubyte;
+typedef          char byte;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long int uint64;
+#endif
